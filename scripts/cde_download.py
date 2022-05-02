d68eb9540a69a57e9867a8304d7c910be36ae673
hackerlikecomputer <hackerlikecomputer@gmail.com>
2022-05-02 16:52:12 -0500

    use python-remotezip library to prevent overwriting existing large files

--- scripts/cde_download.py
+++ scripts/cde_download.py
@@ -1,8 +1,7 @@
 """downloads supplementary homicide report documents from 1985-present to raw"""
 
-from io import BytesIO
-import zipfile
-import requests
+import os
+from remotezip import RemoteZip
 from tqdm import tqdm
 
 BASE_URL = "https://s3-us-gov-west-1.amazonaws.com/cg-d4b776d0-d898-4153-90c8-8336f86bdfec/masters"
@@ -10,12 +9,10 @@ BASE_URL = "https://s3-us-gov-west-1.amazonaws.com/cg-d4b776d0-d898-4153-90c8-83
 FILES = ["shr", "nibrs"]
 
 for year in tqdm(range(1995, 2021), desc="Download files", leave=False):
-    for file in FILES:
-        url = f"{BASE_URL}/{file}/{file}-{year}.zip"
-        resp = requests.get(url)
-
-        if not resp.status_code == 200:
-            raise ValueError(f"Request returned code {resp.status_code}, expected 200")
-
-        with zipfile.ZipFile(BytesIO(resp.content)) as zip_file:
-            zip_file.extractall(f"raw/{file}")
+    for fbi_file in FILES:
+        url = f"{BASE_URL}/{fbi_file}/{fbi_file}-{year}.zip"
+        with RemoteZip(url) as remote_zip:
+            for zipped_file in remote_zip.infolist():
+                dirname = f"raw/{fbi_file}"
+                if not os.path.exists(f"{dirname}/{zipped_file.filename}"):
+                    remote_zip.extract(zipped_file, dirname)
